7cef973f727d879193b22663386cdfef1f2ec4d7
Ivan Hristov <ivan.hristov.biz@gmail.com>
2022-02-06 11:56:14 +0200

    Filter out the junk

--- sentiment/analysis/analyser.py
+++ sentiment/analysis/analyser.py
@@ -1,4 +1,5 @@
-from typing import List
+import re
+from typing import List, Pattern
 
 import spacy
 from spacy.lang.en import English
@@ -7,14 +8,26 @@ from spacy.tokens import Token
 from sentiment.airline import Comment
 
 
+_TAG_REGEX = re.compile(r"^@.*$")
+_URL_REGEX = re.compile(r"^https?:\/\/\S+|www\.\S+$")
+_PUNCT_REGEX = re.compile(r"[^\w\s]")
+_NEWLINE_REGEX = re.compile(r"\n")
+
+
 def train(corpus: List[Comment]):
     nlp = spacy.load("en_core_web_sm")
     cnt = 0
 
     for comment in corpus:
         tokens = tokenize(nlp, comment.text)
+        if cnt == 483:
+            print(tokens)
         tokens = filter_stop_words(tokens)
         tokens = lemmatize(tokens)
+        tokens = filter_junk(tokens)
+
+        if cnt == 483:
+            print(tokens)
 
         cnt += 1
 
@@ -35,3 +48,16 @@ def filter_stop_words(tokens: List[Token]) -> List[Token]:
 
 def lemmatize(tokens: List[Token]) -> List[str]:
     return list(map(lambda token: token.lemma_, tokens))
+
+
+def filter_junk(tokens: List[str]) -> List[str]:
+    tokens = filter_by_regex(_TAG_REGEX, tokens)
+    tokens = filter_by_regex(_URL_REGEX, tokens)
+    tokens = filter_by_regex(_PUNCT_REGEX, tokens)
+    tokens = filter_by_regex(_NEWLINE_REGEX, tokens)
+
+    return tokens
+
+
+def filter_by_regex(regex: Pattern, tokens: List[str]) -> List[str]:
+    return list(filter(lambda token: not regex.search(token), tokens))
