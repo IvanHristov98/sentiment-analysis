a14b589e986752541a396b2b14a51d58adfa1478
Ivan Hristov <ivan.hristov.biz@gmail.com>
2022-02-06 11:10:52 +0200

    Remove stop words

--- sentiment/analysis/analyser.py
+++ sentiment/analysis/analyser.py
@@ -2,22 +2,31 @@ from typing import List
 
 import spacy
 from spacy.lang.en import English
+from spacy.tokens import Token
 
 from sentiment.airline import Comment
 
 
 def train(corpus: List[Comment]):
     nlp = spacy.load("en_core_web_sm")
+    cnt = 0
 
     for comment in corpus:
-        tokenize(nlp, comment.text)
+        tokens = tokenize(nlp, comment.text)
+        tokens = filter_stop_words(tokens)
+
+        cnt += 1
 
 
-def tokenize(nlp: English, text: str) -> List[str]:
+def tokenize(nlp: English, text: str) -> List[Token]:
     doc = nlp(text)
     tokens = []
 
     for token in doc:
-        tokens.append(token.text)
+        tokens.append(token)
 
     return tokens
+
+
+def filter_stop_words(tokens: List[Token]) -> List[Token]:
+    return list(filter(lambda token: not token.is_stop, tokens))
